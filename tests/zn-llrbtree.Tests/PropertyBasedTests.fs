94c8dab9ca4490d046cda0e5818a6c9c2eaafe58
zakaluka <zakthrow@gmail.com>
2020-05-10 12:59:51 -0500

    Added invariant tests
    Signed-off-by: zakaluka <zakthrow@gmail.com>

--- tests/zn-llrbtree.Tests/PropertyBasedTests.fs
+++ tests/zn-llrbtree.Tests/PropertyBasedTests.fs
@@ -5,6 +5,7 @@ open Hedgehog
 open ZN.DataStructures
 
 /// Ensures that the data structure keeps the values sorted / in order.
+[<Tests>]
 let ``Values are in order`` =
   testCase "Values are in order" <| fun _ ->
     property {
@@ -19,6 +20,7 @@ let ``Values are in order`` =
 
 /// Ensures that `min` and `max` find the lowest and highest values,
 /// respectively.
+[<Tests>]
 let ``Min and Max`` =
   testCase "Min and Max" <| fun _ ->
     property {
@@ -37,6 +39,7 @@ let ``Min and Max`` =
     |> Property.check
 
 /// Tests the `ofArray` function.
+[<Tests>]
 let Add =
   testCase "Add" <| fun _ ->
     property {
@@ -52,6 +55,7 @@ let Add =
     |> Property.check
 
 /// Test the `remove` function.
+[<Tests>]
 let Remove =
   testCase "Remove" <| fun _ ->
     property {
@@ -66,6 +70,7 @@ let Remove =
     }
     |> Property.check
 
+[<Tests>]
 let ``Add and Remove`` =
   testCase "Add and Remove" <| fun _ ->
     property {
@@ -88,6 +93,7 @@ let ``Add and Remove`` =
     }
     |> Property.check
 
+[<Tests>]
 let ``Difference with itself`` =
   testCase "Difference with itself" <| fun _ ->
     property {
@@ -101,6 +107,7 @@ let ``Difference with itself`` =
     }
     |> Property.check
 
+[<Tests>]
 let ``Union with itself`` =
   testCase "Union with itself" <| fun _ ->
     property {
@@ -115,6 +122,7 @@ let ``Union with itself`` =
     }
     |> Property.check
 
+[<Tests>]
 let ``tryPick and Pick`` =
   testCase "tryPick and Pick" <| fun _ ->
     property {
@@ -138,7 +146,8 @@ let ``tryPick and Pick`` =
     }
     |> Property.check
 
-let ``Forall`` =
+[<Tests>]
+let Forall =
   testCase "Forall" <| fun _ ->
     property {
       let! g = Gen.array <| Range.exponential 1 5000
@@ -153,7 +162,8 @@ let ``Forall`` =
     }
     |> Property.check
 
-let ``Collect`` =
+[<Tests>]
+let Collect =
   testCase "Collect" <| fun _ ->
     property {
       let! g = Gen.array <| Range.exponential 1 5000
@@ -168,6 +178,7 @@ let ``Collect`` =
     }
     |> Property.check
 
+[<Tests>]
 let ``Intersection with itself`` =
   testCase "Intersection with itself" <| fun _ ->
     property {
@@ -183,32 +194,75 @@ let ``Intersection with itself`` =
     }
     |> Property.check
 
+[<Tests>]
 let ``Fold and FoldBack`` =
   testCase "Fold and FoldBack" <| fun _ ->
     property {
-      let! g= Gen.array<| Range.exponential 1 5000 <| Gen.int (Range.constant System.Int32.MinValue System.Int32.MaxValue)
+      let! g = Gen.array <| Range.exponential 1 5000
+               <| Gen.int
+                    (Range.constant System.Int32.MinValue System.Int32.MaxValue)
       let t = LLRBTree.ofArray g
-      let t1 = LLRBTree.fold (fun acc e -> LLRBTree.add e acc) LLRBTree.empty t
+      let t1 =
+        LLRBTree.fold (fun acc e -> LLRBTree.add e acc) LLRBTree.empty t
       let t2 = LLRBTree.foldBack LLRBTree.add t LLRBTree.empty
 
-      Expect.equal (LLRBTree.toList t1) (LLRBTree.toList t2) "Fold and FoldBack"
+      Expect.equal (LLRBTree.toList t1) (LLRBTree.toList t2)
+        "Fold and FoldBack"
+    }
+    |> Property.check
+
+/// https://www.geeksforgeeks.org/left-leaning-red-black-tree-insertion/
+[<Tests>]
+let ``Invariant 1 Root node is always black`` =
+  testCase "Invariant 1 Root node is always black" <| fun _->
+    property {
+      let! g = Gen.array <| Range.exponential 0 5000 <| Gen.int (Range.constant System.Int32.MinValue System.Int32.MaxValue)
+
+      Expect.equal (LLRBTree.ofArray g|> LLRBTree.getColor ) Color.B "Root node of PB tree is black"
+      Expect.equal (LLRBTree.getColor LLRBTree.empty ) Color.B "Root node of empty tree is black"
+      Expect.equal (LLRBTree.singleton 'x' |> LLRBTree.getColor) Color.B "Root of singleton is black"
     }
     |> Property.check
 
 [<Tests>]
-let addTests =
-  testList "Property-Based Tests"
-    [
-      Add
-      ``Add and Remove``
-      Collect
-      ``Fold and FoldBack``
-      Forall
-      ``Difference with itself``
-      ``Intersection with itself``
-      ``Min and Max``
-      Remove
-      ``tryPick and Pick``
-      ``Union with itself``
-      ``Values are in order``
-       ]
+let ``Invariant 4 Node cannot have Left Black and Right Red children`` =
+  // True = node does not have left black and right red children
+  let childChecker t=
+    match t with
+    | E -> true
+    | T(_,l,_,r) -> (LLRBTree.getColor l = B && LLRBTree.getColor r = R) |> not
+
+  // Walks through the tree and performs operations on nodes instead of values
+  let rec foldNodes func acc t =
+    match t with
+    | E -> acc
+    | T(_,l,_,r) -> foldNodes func (foldNodes func (func acc t) l) r
+
+  testCase "Invariant 4 Node cannot have Left Black and Right Red children" <| fun _->
+    property {
+      let! g = Gen.array <| Range.exponential 0 5000 <| Gen.int (Range.constant System.Int32.MinValue System.Int32.MaxValue)
+      let result =
+        LLRBTree.ofArray g
+        |> foldNodes (fun acc e -> acc && (childChecker e)) true
+
+      Expect.isTrue result "Invariant 4"
+    } |> Property.check
+
+[<Tests>]
+let ``fold and fold' have the same behavior`` =
+  testCase "fold and fold' for all integers" <| fun _ ->
+    property {
+      let! g = Gen.array <| Range.exponential 0 5000 <| Gen.int (Range.constant System.Int32.MinValue (System.Int32.MaxValue-1))
+      let t= LLRBTree.ofArray g
+
+      let t1 =
+        LLRBTree.fold (fun acc e -> LLRBTree.add e acc) LLRBTree.empty t
+      let t2 = LLRBTree.fold' (fun acc e -> LLRBTree.add e acc) LLRBTree.empty t
+
+      Expect.equal (LLRBTree.toList t1) (LLRBTree.toList t2) "List from trees are identical"
+      Expect.equal t1 t2 "Trees are identical"
+
+      let t3 = LLRBTree.fold (fun acc e -> (e+1)::acc) [] t
+      let t4 = LLRBTree.fold' (fun acc e -> (e+1)::acc) [] t
+      Expect.equal t3 t4 "Lists based on +1 are identical"
+    }|>Property.check
