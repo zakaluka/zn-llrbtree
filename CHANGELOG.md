74bd810515b52600144dc856f2201da7883fccc3
zakaluka <zakthrow@gmail.com>
2020-05-11 14:00:55 -0500

    Bump version to 0.2.0
    ## [0.2.0] - 2020-05-11
    
    Remediated property-based tests.
    
    ### Added
    - Added `fold'` - heap-based folding function.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,19 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.2.0] - 2020-05-11
+
+Remediated property-based tests.
+
+### Added
+- Added `fold'` - heap-based folding function.
 
 ## [0.1.0] - 2020-03-24
+
 First release.  Based on the Elm implementation at https://github.com/Skinney/core/blob/a2ae7fcc30bd1ca2d5c732f98f61e4fd1f7b288b/src/Dict.elm
 
 ### Added
@@ -14,4 +21,6 @@ First release.  Based on the Elm implementation at https://github.com/Skinney/co
 - Unit tests for basic functionality
 - Property-based tests
 
+[Unreleased]: https://github.com/zakaluka/zn-llrbtree/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/zakaluka/zn-llrbtree/compare/v0.1.0...v0.2.0
 [0.1.0]: https://github.com/zakaluka/zn-llrbtree.git/releases/tag/v0.1.0
--- src/zn-llrbtree/AssemblyInfo.fs
+++ src/zn-llrbtree/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("zn-llrbtree")>]
 [<assembly: AssemblyProductAttribute("zn-llrbtree")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-05-11T00:00:00.0000000-05:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","90b58c897f353032f339c6b78d3caf30b084cca3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "zn-llrbtree"
     let [<Literal>] AssemblyProduct = "zn-llrbtree"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.2.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-05-11T00:00:00.0000000-05:00"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "90b58c897f353032f339c6b78d3caf30b084cca3"
